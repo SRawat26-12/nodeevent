95c8ead968df7de1a6fd3f69428a611ba1f7e95b
shivam <shivamrawat1053@gmail.com>
2024-10-13 15:42:36 +0530

    events

---
+++ app.js
@@ -0,0 +1,30 @@
+/*const EventEmitter=require('events');
+const event=new EventEmitter();
+event.on("say",()=>{
+    console.log("your name is raj");
+})
+event.emit('say');*/
+
+/*const EventEmitter=require('events');//mutiple function call by using single event
+const event=new EventEmitter();
+event.on("say",()=>{
+    console.log("your name is raj");
+})
+event.on("say",()=>{
+    console.log("your sirname is rawat");
+})
+event.on("say",()=>{
+    console.log("your city is bhopal");
+})
+event.emit('say');*/
+
+/*const EventEmitter=require('events');
+const event=new EventEmitter();
+ event.on("checkpage",(sc,msg)=>{
+    console.log(`status code is ${sc} and the page is ${msg}`);
+})
+event.emit('checkpage',200,"ok");*/
+
+
+
+
